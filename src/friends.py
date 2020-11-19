9cfae7a8881d12a42f4587ffcf3186ddc939d22f
evelyn-sz <evelynszarek@gmail.com>
2020-11-19 11:45:17 +0000

    test 3 done

--- src/friends.py
+++ src/friends.py
@@ -5,3 +5,7 @@ def get_name(person):
 #2
 def get_favourite_tv_show(person):
     return person["favourites"]["tv_show"]
+
+#3
+def likes_to_eat(person, food):
+    return food in person['favourites']['snacks']
