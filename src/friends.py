39503617ecfe8901ae6844d01029497cac1afeef
evelyn-sz <evelynszarek@gmail.com>
2020-11-19 12:05:19 +0000

    Test 4

--- src/friends.py
+++ src/friends.py
@@ -9,3 +9,8 @@ def get_favourite_tv_show(person):
 #3
 def likes_to_eat(person, food):
     return food in person['favourites']['snacks']
+
+
+def add_friend(person, friend):
+    person["friends"].append(friend)
+    return len(person["friends"])
