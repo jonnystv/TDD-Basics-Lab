5f58304f7d0c581c86ce70db3a53a39436fe1364
evelyn-sz <evelynszarek@gmail.com>
2020-11-19 13:25:32 +0000

    exercise 6

--- src/friends.py
+++ src/friends.py
@@ -10,7 +10,21 @@ def get_favourite_tv_show(person):
 def likes_to_eat(person, food):
     return food in person['favourites']['snacks']
 
-
+#4
 def add_friend(person, friend):
     person["friends"].append(friend)
-    return len(person["friends"])
+#return len(person["friends"])
+
+
+#5
+def remove_friend(person, friend):
+    person["friends"].remove(friend)
+
+#6
+
+def total_money(people_dictionaries):
+    money = 0
+    for person in people_dictionaries:
+        money += person['monies']
+    return money
+
--- tests/friends_test.py
+++ tests/friends_test.py
@@ -99,16 +99,17 @@ class TestFriends(unittest.TestCase):
 
   # 5. For a given person, remove a specific name from their list of friends
   # (hint: Same as above, testing for the length of the array should be sufficient)
-  @unittest.skip("delete this line to run the test")
+  # @unittest.skip("delete this line to run the test")
   def test_remove_friend(self):
     add_friend(self.person2, "Scrappy-Doo")
     remove_friend(self.person2, "Fred")
     self.assertEqual(1, len(self.person2["friends"]))
 
 
+
   # 6. Find the total of everyone's money
   # (hint: use the self.people array, remember how we checked the total number of eggs yesterday?)
-  @unittest.skip("delete this line to run the test")
+  #@unittest.skip("delete this line to run the test")
   def test_total_money(self):
     self.assertEqual(143, total_money(self.people))
 
